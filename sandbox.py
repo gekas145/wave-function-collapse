6f1ca2fdb5ee5388909bed8e5cf3c5b70255a628
gekas145 <56126336+gekas145@users.noreply.github.com>
2023-08-11 22:38:11 +0200

    start adjacency rules

--- sandbox.py
+++ sandbox.py
@@ -2,17 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2
 from wfc.preprocessor import Preprocessor
+from wfc.utils import Direction
 
 prep = Preprocessor(8, 4)
 
 prep._preprocess_tiles("images/test-image.png")
 
-for tile in prep.tiles:
-    plt.imshow(tile.tile)
-    plt.title(tile.count)
-    plt.show()
+# for tile in prep.tiles[:2]:
+#     plt.imshow(tile.tile[:, :, :])
+#     plt.title(tile.count)
+#     plt.show()
 
 
 
+print(prep.tiles[0].is_compatible(prep.tiles[1], Direction.RIGHT))
 
 
--- wfc/utils.py
+++ wfc/utils.py
@@ -1,9 +1,10 @@
+import numpy as np
 from enum import Enum
 
 
 class Direction(Enum):
 
-    UP = 0
+    TOP = 0
     RIGHT = 1
     DOWN = 2
     LEFT = 3
@@ -17,4 +18,12 @@ class Tile:
         self.pixel_size = pixel_size
 
     def is_compatible(self, other, direction):
-        pass
+        tile_shape = self.tile.shape[0]
+
+        if direction == Direction.TOP:
+            return np.array_equal(self.tile[self.pixel_size:, :, :],
+                                  other.tile[:tile_shape - self.pixel_size, :, :])
+
+        # else check right adjacency
+        return np.array_equal(self.tile[:, self.pixel_size:, :],
+                              other.tile[:, :tile_shape - self.pixel_size, :])
