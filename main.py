f4fb370407216c52c90442766f2de999a70b6fac
gekas145 <56126336+gekas145@users.noreply.github.com>
2023-08-27 11:24:42 +0200

    small corrections

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ from wfc.utils import Direction, GridCell
 
 pixel_size = 10
 window_size = 3
-output_shape = (40, 150)
+output_shape = (40, 160)
 retry_num = 5
 max_count = 20
 input_im_path = "images/spirals.png"
-output_im_path = "generated_images/spirals1.png"
+output_im_path = "generated_images/spirals.png"
 
 prep = Preprocessor(pixel_size, window_size, max_count)
 prep.preprocess(input_im_path)
